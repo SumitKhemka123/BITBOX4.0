ce8c0eeb2576c06a2359712d4248b840f762f019
Anshul Kansal <anshulkansal89@gmail.com>
2024-03-13 13:42:58 +0530

    Added WebRTC

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SocketProvider } from "../src/components/context/SocketProvider";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <SocketProvider>
     <App />
+    </SocketProvider>
+   
   </React.StrictMode>
 );
--- server/index.js
+++ server/index.js
@@ -1,21 +1,38 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import pg from 'pg';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import {Server} from 'socket.io';
 
-const app = express();
-const PORT=3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("client"));
+const io = new Server(8000, {
+  cors: true,
+});
 
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
 
-app.get("/" , (res, req)=>{
-    req.send("Hello World");
-})
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
 
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
 
-app.listen(PORT , ()=>{
-    console.log("Server is running on PORT 3000");
-})
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
